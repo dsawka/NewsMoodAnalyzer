94bbd38c22cce3efecc7ad07745781cbadbcfd50
dsawka <sawkadamian@gmail.com>
2023-04-30 12:07:05 +0200

    Initial commit

---
+++ src/test/java/pl/coderslab/newsmoodanalyzer/NewsMoodAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.newsmoodanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsMoodAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
